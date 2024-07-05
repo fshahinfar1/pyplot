e33d46eb957013515aac204b8df44dd2c61403e2
Farbod Shahinfar <fshahinfar1@gmail.com>
2024-07-05 15:51:24 +0200

    update cdf plotting script

--- tools/cdf.py
+++ tools/cdf.py
@@ -1,54 +1,102 @@
+import os
+import sys
 import yaml
 import matplotlib.pyplot as plt
-import argparse
-import numpy as np
+from matplotlib.lines import Line2D
 import common_matplot_config
 
 
-def parse():
-    parser = argparse.ArgumentParser('CDF plot tool')
-    parser.add_argument('config_file', type=str, help='path to config file')
-    args = parser.parse_args()
-    return args
+def plot_a_line(ax, line):
+    """
+    @ax:
+    @line: config for a line
+    """
+    tmp = line.copy()
+    del tmp['data']
+
+    data = list(line['data'])
+    if 'scale' in line:
+        del tmp['scale']
+        s = line['scale']
+        for i in range(len(y)):
+            data[i] *= s
+    line = ax.ecdf(data, **tmp)
 
 
-def ecdf(x, n):
-    x = sorted(x)
-    def _ecdf(v):
-        # side='right' because we want Pr(x <= v)
-        return (np.searchsorted(x, v, side='right') ) / n
-    return _ecdf
+file_path = sys.argv[1]
+with open(file_path, 'r') as f:
+    config = yaml.safe_load(f)
 
+fig = plt.figure(figsize=config['figsize'])
+cols = len(config['subplots'])
+for i in range (cols):
+    subconf = config['subplots'][i]
+    ax = fig.add_subplot(1,cols,i+1)
+    for line in subconf['lines']:
+        plot_a_line(ax, line)
 
-def main():
-    args = parse()
-    with open(args.config_file) as f:
-        conf = yaml.safe_load(f)
+    if 'ylabel' in subconf and subconf['ylabel']:
+        ax.set_ylabel(subconf['ylabel'])
+    if 'xlabel' in subconf and subconf['xlabel']:
+        ax.set_xlabel(subconf['xlabel'])
+    if 'ylim' in subconf and subconf['ylim']:
+        ax.set_ylim(subconf['ylim'])
+    if 'xlim' in subconf and subconf['xlim']:
+        ax.set_xlim(subconf['xlim'])
+    if 'xticks' in subconf and subconf['xticks']:
+        ax.xaxis.set_ticks(subconf['xticks'])
+    if 'xtick_labels' in subconf and subconf['xtick_labels']:
+        ax.xaxis.set_ticklabels(subconf['xtick_labels'])
+    if 'yticks' in subconf and subconf['yticks']:
+        ax.yaxis.set_ticks(subconf['yticks'])
+    if 'ytick_labels' in subconf and subconf['ytick_labels']:
+        ax.yaxis.set_ticklabels(subconf['ytick_labels'])
 
-    figsize = conf.get('figsize')
-    fig = plt.figure(figsize = figsize)
-    ax = fig.add_subplot(1,1,1)
-    for data in conf.get('data', tuple()):
-        if 'file' not in data:
-            raise Exception('field with the name of "file" not found in config file')
-        d = np.loadtxt(data['file'])
-        k = len(d)
-        _max = np.max(d)
-        x = np.arange(0, _max, (_max - 0) / 1000)
-        y = ecdf(d, k)(x)
-        if 'plot_params' in data:
-            params = data['plot_params']
+    if ('xlabel_rotation_deg' in subconf and
+            subconf['xlabel_rotation_deg']):
+        ax.tick_params(axis="x",
+                rotation=subconf['xlabel_rotation_deg'])
+
+    if 'xlogscale' in subconf:
+        ax.set_xscale('log', base=subconf['xlogscale'])
+
+    if 'legend' in subconf and subconf['legend']:
+        if isinstance(subconf['legend'], dict):
+            if subconf['legend'].get('outside', False):
+                subconf['legend'].pop('outside')
+                fig.legend(**subconf['legend'])
+            else:
+                ax.legend(**subconf['legend'])
         else:
-            params = {}
-        ax.plot(x, y, **params)
+            ax.legend()
 
-    fig.tight_layout()
-    ax.legend()
-    ax.grid(True)
-    if 'output' in conf:
-        plt.savefig(conf['output'], dpi=300)
-    plt.show()
+    ax.set_axisbelow(True)
+    ax.grid()
+    # ax.legend()
+    if 'title' in subconf and subconf['title']:
+        ax.set_title(subconf['title'])
 
+if 'suptitle' in config and config['suptitle']:
+    plt.suptitle(config['suptitle'])
+if 'rc' in config:
+    for param in config['rc']:
+        tmp = param.copy()
+        del tmp['name']
+        plt.rc(param['name'], **tmp)
 
-if __name__ == '__main__':
-    main()
+# draw annotations
+for a in config.get('annotate', []):
+    plt.annotate(**a)
+for a in config.get('arrow', []):
+    plt.arrow(**a)
+
+# padding=config.get('tight_layout', {})
+# plt.tight_layout(**padding)
+plt.tight_layout()
+
+outdir = './out'
+if not os.path.isdir(outdir):
+    os.mkdir(outdir)
+output_file = os.path.join(outdir, config['output'])
+plt.savefig(output_file, dpi=300, bbox_inches='tight',pad_inches = 0)
+plt.show()
