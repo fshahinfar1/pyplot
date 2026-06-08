a3e5dc668f902f73c345ad50cc6aff401318a9a3
Farbod Shahinfar <fshahinfar1@gmail.com>
2026-06-08 22:23:53 +0200

    improve the operatbility with venv

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
-curdir=$(dirname $0)
-install_dir=/opt/pyplot
-sudo mkdir -p $install_dir
-sudo cp -r $curdir/tools/ $install_dir/
-# sudo cp $curdir/pyplot /usr/bin/pyplot
+curdir=$(dirname "$0")
+install_dir="/opt/pyplot"
+sudo mkdir -p "$install_dir"
+sudo cp -r "$curdir/tools/" "$install_dir/"
+
+if [ "$OSTYPE" = "linux-gnu" ]; then
+	# NOTE: on Mac, I'm not installing this here, but manually placing it in a
+	# local bin folder
+	sudo cp "$curdir"/pyplot /usr/bin/pyplot
+fi
